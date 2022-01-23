f7182ecf04d1f604cb5ed9177e68e5c613f3a317
vinodjipande2022 <98266094+vinodjipande2022@users.noreply.github.com>
2022-01-23 19:05:48 +0530

    Arithmetic
    This is basic java Arithmetic Operation

---
+++ Arithmetic.java
@@ -0,0 +1,25 @@
+class Arithmetic
+{
+public static void main(String args[])
+{
+int a,b,c;
+a=22;
+b=11;
+System.out.println("A="+a);
+System.out.println("B="+b);
+c=a+b;
+System.out.println("Addition="+c);
+c=a-b;
+System.out.println("Substraction="+c);
+c=a*b;
+System.out.println("Miltiplication="+c);
+c=a/b;
+System.out.println("Divide="+c);
+c=a%b;
+System.out.println("Reminder="+c);
+
+}
+}
+
+
+
